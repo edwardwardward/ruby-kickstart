b67253a90f874e3b7721e6db4739bbb764865acd
Edward Ward <edwardwardward@gmail.com>
2017-02-21 20:45:10 +0000

    Add 1-6 of session2

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -7,3 +7,12 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+
+
+
+
+def get_squares(nu)
+  combined_squares = []
+  squares = nu.map { |num| num * num }
+  nu.select { |n| nu.include? n*n }.sort
+end
